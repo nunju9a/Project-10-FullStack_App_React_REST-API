3705a17f90c04a42376fc7e166efdd8b2330a594
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-09-11 08:10:14 -0400

    created api folder with project 9 files

---
+++ api/routes/home.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+//Setup a friendly greeting for the root route
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the REST API project!',
+  });
+});
+
+module.exports = router;
