1677d040469d33c13c34ab1c0efae11e958e6259
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-09-12 07:46:01 -0400

    tested call to api for courses - successful

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+// import config from './config'; 
+class App extends React.Component {
+  state = {
+    courses : [],
+    isLoading : true,
+  }
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/courses`)
+      .then(response => response.json())
+      .then(courses => this.setState({
+        courses : courses,
+        isLoading : false
+      }))
+      .then(() => console.log(this.state.courses))
+      .catch(err => console.log("There was an error loading the page: " + err))
+  }
+  render() {
+    return this.state.isLoading ? (<h2>Loading...</h2>) : (
+      <div className="container">
+        {
+          this.state.courses.map(course => {
+            return (
+              <ul key={course.id}>
+                <li>{course.id}</li>
+                <li>{course.title}</li>
+                <li>{course.description}</li>
+                <li>{course.estimatedTime}</li>
+                <li>{course.materialsNeeded}</li>
+              </ul>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
